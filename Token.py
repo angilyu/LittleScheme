4b55fb9436027f3e933a2df9d9001274f2f8822a
Wenjing Yu <angilyu@gmail.com>
2012-06-12 17:28:07 -0400

    move Tokens enum to the Token.py

--- Token.py
+++ Token.py
@@ -1,11 +1,40 @@
 
+# Constants for Tokens
+class Tokens:
+    # -- Characters
+    LPAREN = 1 # "("
+    RPAREN = 2 # ")"
+    QUOTE = 3 # "'"
+
+    # -- Primitives
+    # text enclosed by ", can use \" to escape internal `double quote`
+    STRING = 10 # "example"
+    TRUE = 11 # #t
+    FALSE = 12 # #f
+    # ------
+    NUMBER = 13
+    # variable could be composed by alphabets (a-z, A-Z) and digits (0~9)
+    # or special characters: ! $ % & * + - . / : < = > ? @ ^ _ ~
+    VARIABLE = 14
+
+    # -- Keywords
+    DEFINE = 20 # define
+    COND = 21 # cond
+    IF = 22 # if
+    ELSE = 23 # else
+    ASSIGNMENT = 24 # set!
+    NULL = 25 # NULL
+
+    # -- Undefined
+    UNDEFINED = 30
+
 class Token:
-    def __init__(self, tokenType, pos, content = None):
+    def __init__(self, tokenType, pos, literal = None):
         """ the Token instance has the follow fields:
              @params tokenType indicats the type of this token
              @params pos indicates the start position in the original file
-             @params content is optional, contains the content of the token 
+             @params literal is optional, contains the literal of the token
          """
         self.tokenType = tokenType
         self.pos = pos
-        self.content = content
+        self.literal = literal
