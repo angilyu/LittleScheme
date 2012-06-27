dc20f7556a796bf3416dc393236197128292dba7
Wenjing Yu <angilyu@gmail.com>
2012-06-27 02:22:20 -0400

    Add a multiple expression test case.

--- test/ParseTest.py
+++ test/ParseTest.py
@@ -1,7 +1,7 @@
 import TestConfig
 import unittest
 import Tokenize
-import Parse
+from Parse import *
 from Token import *
 from Exp import *
 from Value import *
@@ -14,9 +14,9 @@ class ParseTest(unittest.TestCase):
                     makeBoolean(True), makeBoolean(False),
                     makeSymbol("symbol"), makeNULL()]
 
-        result = list(Parse.parse(tokens))
+        result = list(parse(tokens))
 
-        self.assertTrue(all(code == Parse._OK for code, _ in result))
+        self.assertTrue(all(code == ParseError.OK for code, _ in result))
         actual = [item for _, item in result]
         self.assertEqual(expected, actual)
 
@@ -27,24 +27,37 @@ class ParseTest(unittest.TestCase):
                     makeKeyword(Tokens.IF), makeKeyword(Tokens.ELSE),
                     makeKeyword(Tokens.ASSIGNMENT), makeKeyword(Tokens.LAMBDA)]
 
-        result = list(Parse.parse(tokens))
+        result = list(parse(tokens))
 
-        self.assertTrue(all(code == Parse._OK for code, _ in result))
+        self.assertTrue(all(code == ParseError.OK for code, _ in result))
         actual = [item for _, item in result]
         self.assertEqual(expected, actual)
 
-    def test_single_epression(self):
+    def test_single_expression(self):
         tokens = Tokenize.tokenize("(define size 4)", 0)
         expList = [makeKeyword(Tokens.DEFINE), makeSymbol("size"), makeNumber(4)]
         expected = makeList(expList)
 
-        result = list(Parse.parse(tokens))
+        result = list(parse(tokens))
 
-        self.assertTrue(all(code == Parse._OK for code, _ in result))
+        self.assertTrue(all(code == ParseError.OK for code, _ in result))
         actual = [item for _, item in result]
         self.assertEqual(1, len(actual))
-        print expected, actual[0]
         self.assertEqual(expected, actual[0])
+    def test_multiple_expression(self):
+        tokens = Tokenize.tokenize("(define size 4)(+ size 5)", 0)
+        expListOne = [makeKeyword(Tokens.DEFINE), makeSymbol("size"), makeNumber(4)]
+        expListTwo = [makeSymbol("+"), makeSymbol("size"), makeNumber(5)]
+        expectedOne = makeList(expListOne)
+        expectedTwo = makeList(expListTwo)
+
+        result = list(parse(tokens))
+
+        self.assertTrue(all(code == ParseError.OK for code, _ in result))
+        actual = [item for _, item in result]
+        self.assertEqual(2, len(actual))
+        self.assertEqual(expectedOne, actual[0])
+        self.assertEqual(expectedTwo, actual[1])
 
 if __name__ == '__main__':
     unittest.main()
